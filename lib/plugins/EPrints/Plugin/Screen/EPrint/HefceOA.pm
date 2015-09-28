fe7b32f88b29ce8df61d06b690bd3fcfb055044c
Timothy <timothy.miles-board@london.ac.uk>
2015-09-28 10:15:25 +0100

    Rework compliance screen to show tabs for deposit requirements, discovery requirements, access requirements and exceptions

--- lib/plugins/EPrints/Plugin/Screen/EPrint/HefceOA.pm
+++ lib/plugins/EPrints/Plugin/Screen/EPrint/HefceOA.pm
@@ -25,7 +25,7 @@ sub can_be_viewed
 {
 	my( $self ) = @_;
 
-    return unless $self->{processor}->{eprint}->is_set( "hoa_compliant" );
+	return unless $self->{processor}->{eprint}->is_set( "hoa_compliant" );
 
 	return $self->allow( "eprint/hefce_oa" );
 }
@@ -38,45 +38,118 @@ sub render
 	my $page = $repo->xml->create_element( "div" ); # wrapper
 
 	my $eprint = $self->{processor}->{eprint};
-    my $flag = $eprint->value( "hoa_compliant" );
+	my $flag = $eprint->value( "hoa_compliant" ) || 0;
 
-    for(qw(
-        COMPLIANT
-        DEP
-        DEP_TIMING
-        DEP_AAM
-        DIS
-        DIS_DISCOVERABLE
-        ACC
-        ACC_TIMING
-        ACC_EMBARGO
-        EX_DEP
-        EX_ACC
-        EX_TEC
-        EX_OTH
-    ))
-    {
-        my $h = $repo->xml->create_element( "h2" );
-        $h->appendChild( $repo->call( [qw( hefce_oa render_test_title )], $repo, $_ ) );
-        $page->appendChild( $h );
-
-        if( $flag & HefceOA::Const->$_ )
-        {
-            $page->appendChild( $repo->render_message( "message", $self->html_phrase( "test:compliant" ) ) );
+	# overall compliance
+	if( $flag & HefceOA::Const::COMPLIANT )
+	{
+            $page->appendChild( $repo->render_message( "message", $self->html_phrase( "compliant" ) ) );
         }
         else
         {
-            $page->appendChild( $repo->render_message( "warning", $self->html_phrase( "test:not_compliant" ) ) );
+            $page->appendChild( $repo->render_message( "warning", $self->html_phrase( "non_compliant" ) ) );
         }
+	$page->appendChild( $self->html_phrase( "render_test_description",
+		description => $repo->html_phrase( "hefce_oa:test_description:COMPLIANT" )
+	) );
 
-        $page->appenChild( $repo->xml->create_element( "br" ) );
-        $page->appendChild( $repo->call( [qw( hefce_oa render_test_details )], $repo, $_, $eprint ) );
+	# tabs for individual requirements and exceptions
+	my @labels;
+	my @tabs;
+	for(
+		[ "DEP", [qw( DEP_TIMING DEP_AAM )] ],
+		[ "DIS", [qw( DIS_DISCOVERABLE )] ],
+		[ "ACC", [qw( ACC_TIMING ACC_EMBARGO )] ],
+		[ "EX", [qw( EX_DEP EX_ACC EX_TEC EX_OTH )] ],
+	)
+	{
+		my( $label, $tab ) = $self->render_tab( @$_ );
+		push @labels, $label;
+		push @tabs, $tab;
+	}
+	$page->appendChild( $repo->xhtml->tabs(
+		\@labels,
+		\@tabs,
+		basename => "hoa_tabs",
+	) );
 
-        $page->appenChild( $repo->xml->create_element( "br" ) );
-        $page->appendChild( $repo->call( [qw( hefce_oa render_test_policy )], $repo, $_, $eprint ) );
-    }
+	return $page;
+}
 
-    return $page;
+
+sub render_tab
+{
+	my( $self, $title, $tests ) = @_;
+
+	return $self->render_exceptions_tab( $title, $tests ) if $title eq "EX";
+
+	my $repo = $self->{repository};
+	my $tab = $repo->xml->create_document_fragment;
+
+	my $eprint = $self->{processor}->{eprint};
+	my $flag = $eprint->value( "hoa_compliant" ) || 0;
+
+	my $tab_title = $self->html_phrase( "render_tab_title",
+		title => $repo->html_phrase( "hefce_oa:test_title:$title" ),
+		class => $repo->xml->create_text_node( $flag & HefceOA::Const->$title ? "hoa_compliant" : "hoa_non_compliant" )
+	);
+
+	$tab->appendChild( $self->html_phrase( "render_test_description",
+		description => $repo->html_phrase( "hefce_oa:test_description:$title" )
+	) );
+
+	my $sub = $repo->xml->create_document_fragment;
+	for( @$tests )
+	{
+		$sub->appendChild( $self->html_phrase( "render_test",
+			title => $repo->html_phrase( "hefce_oa:test_title:$_" ),
+			description => $repo->html_phrase( "hefce_oa:test_description:$_" ),
+			class => $repo->xml->create_text_node( $flag & HefceOA::Const->$_ ? "hoa_compliant" : "hoa_non_compliant" )
+		) );
+	}
+	$tab->appendChild( $self->html_phrase( "render_tests", tests => $sub ) );
+
+	return( $tab_title, $tab );
+}
+
+sub render_exceptions_tab
+{
+	my( $self, $title, $tests ) = @_;
+
+	my $repo = $self->{repository};
+	my $tab = $repo->xml->create_document_fragment;
+
+	my $eprint = $self->{processor}->{eprint};
+	my $flag = $eprint->value( "hoa_compliant" ) || 0;
+
+	my $count = 0;
+	unless( $flag & HefceOA::Const::EX )
+	{
+		$tab->appendChild( $self->html_phrase( "render_no_exceptions" ) );
+	}
+	else
+	{
+		my $ex = $repo->xml->create_document_fragment;
+		for( qw( hoa_ex_dep hoa_ex_acc hoa_ex_tec hoa_ex_oth ) )
+		{
+			if( $eprint->is_set( $_ ) )
+			{
+				$ex->appendChild( $self->html_phrase( "render_exception", 
+					title => $repo->html_phrase( "eprint_fieldname_$_" ),
+					exception => $eprint->render_value( $_ ),
+					explanation => $eprint->render_value( "$_\_txt" ),
+				) );
+				$count++;
+			}
+		}
+		$tab->appendChild( $self->html_phrase( "render_exceptions", exceptions => $ex ) );
+	}
+
+	my $tab_title = $self->html_phrase( "render_exception_tab_title",
+		count => $repo->xml->create_text_node( $count ),
+	);
+
+	return( $tab_title, $tab );
 }
 
 1;

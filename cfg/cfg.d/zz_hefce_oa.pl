c286d96fc819f776807fb7d3c095a6572d821968
Timothy <timothy.miles-board@london.ac.uk>
2015-09-28 10:08:09 +0100

    Move user_roles to cfg level

--- cfg/cfg.d/zz_hefce_oa.pl
+++ cfg/cfg.d/zz_hefce_oa.pl
@@ -1,1 +1,4 @@
 $c->{plugins}{"Screen::EPrint::HefceOA"}{params}{disable} = 0;
+
+push @{ $c->{user_roles}->{editor} }, qw{ +eprint/hefce_oa };
+push @{ $c->{user_roles}->{admin} }, qw{ +eprint/hefce_oa };
--- lib/cfg.d/hefce_oa.pl
+++
@@ -1,2 +0,0 @@
-push @{ $c->{user_roles}->{editor} }, qw{ +eprint/hefce_oa };
-push @{ $c->{user_roles}->{admin} }, qw{ +eprint/hefce_oa };

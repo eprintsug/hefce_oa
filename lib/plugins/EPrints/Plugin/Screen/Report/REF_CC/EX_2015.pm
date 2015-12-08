01f11657df18e34bfd1fad0b431e9d63cf60e312
Will Fyson <williamfyson89@hotmail.com>
2015-12-08 15:41:12 +0000

    Fix to exceptions display in report.

--- lib/plugins/EPrints/Plugin/Screen/Report/REF_CC/EX_2015.pm
+++ lib/plugins/EPrints/Plugin/Screen/Report/REF_CC/EX_2015.pm
@@ -39,10 +39,10 @@ sub validate_dataobj
 	{
 		if( $eprint->is_set( $_ ) )
 		{
-			push @problems, $repo->html_phrase( "Plugin/Screen/EPrint/HefceOA:render_exception", 
-				title => $repo->html_phrase( "eprint_fieldname_$_" ),
-				exception => $eprint->render_value( $_ ),
-				explanation => $eprint->render_value( "$_\_txt" ),
+			push @problems, $repo->phrase( "Plugin/Screen/EPrint/HefceOA:render_exception", 
+				title => "<b>" . $repo->phrase( "eprint_fieldname_$_" ) . "</b>",
+				exception => EPrints::Utils::tree_to_utf8( $eprint->render_value( $_ ) ),
+				explanation => EPrints::Utils::tree_to_utf8( $eprint->render_value( "$_\_txt" ) ),
 			);
 		}
 	}

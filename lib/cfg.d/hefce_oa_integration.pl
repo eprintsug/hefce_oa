09ac7c808b39cd85e548920045b7b59aa46bf954
jesusbagpuss <j.salter@leeds.ac.uk>
2016-03-01 16:06:11 +0000

    Map published_online date in preference to published date
    Update correlates to datesdatesdates v1.0.5

--- lib/cfg.d/hefce_oa_integration.pl
+++ lib/cfg.d/hefce_oa_integration.pl
@@ -7,45 +7,70 @@ $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 	# trigger only applies to repos with hefce_oa plugin enabled
 	return unless $eprint->dataset->has_field( 'hoa_compliant' );
 
-    # default
-    if( $eprint->is_set( 'date' ) )
-    {
-        if( $eprint->is_set( 'date_type' ) && $eprint->value( 'date_type' ) eq 'published' )
-        {
-            $eprint->set_value( 'hoa_date_pub', $eprint->value( 'date' ) );
-        }
-    }
+	my( $acc_date, $pub_date, $pub_online_date );
+		
+	# default EPrints date field - only one of these will match
+	if( $eprint->is_set( 'date' ) && $eprint->is_set( 'date_type' ) )
+	{
+		if( $eprint->value( 'date_type' ) eq 'published' )
+		{
+			$pub_date = $eprint->value( 'date' );
+		}
+		elsif( $eprint->value( 'date_type' ) eq 'published_online' )
+		{
+			$pub_online_date = $eprint->value( 'date' );
+		}
+		elsif( $eprint->value( 'date_type' ) eq 'accepted' ) {
+			$acc_date = $eprint->value( 'date' );
+		}
+	}
 
-    # datesdatesdates
-    if( $eprint->exists_and_set( 'dates' ) )
-    {
-        my $dates = $eprint->value( 'dates' );
-        if( ref($dates) eq 'ARRAY' ) # check for expected structure
-        {
-            for(@$dates)
-            {
-                next unless ref($_) eq 'HASH';
-                next unless EPrints::Utils::is_set( $_->{date} ) && EPrints::Utils::is_set( defined $_->{date_type} );
-                if( $_->{date_type} eq 'published' )
-                {
-                    $eprint->set_value( 'hoa_date_pub', $_->{date} );
-                }
-                if( $_->{date_type} eq 'accepted' )
-                {
-                    $eprint->set_value( 'hoa_date_acc', $_->{date} );
-                }
-            }
-        }
-    }
+	# datesdatesdates - v.1.0.5 with 'published_online' option
+	if( $eprint->exists_and_set( 'dates' ) )
+	{
+		my $dates = $eprint->value( 'dates' );
+		if( ref($dates) eq 'ARRAY' ) # check for expected structure
+		{
+			for(@$dates)
+			{
+				next unless ref($_) eq 'HASH';
+				next unless EPrints::Utils::is_set( $_->{date} ) && EPrints::Utils::is_set( defined $_->{date_type} );
+				if( $_->{date_type} eq 'published' )
+				{
+					$pub_date = $_->{date};
+				}
+				elsif( $_->{date_type} eq 'published_online' )
+				{
+					$pub_online_date = $_->{date};
+				}
+				elsif( $_->{date_type} eq 'accepted' )
+				{
+					$acc_date = $_->{date};
+				}
+			}
+		}
+	}
 
-    # rioxx2
-    if( $eprint->exists_and_set( 'rioxx2_dateAccepted' ) )
-    {
-        $eprint->set_value( 'hoa_date_acc', $eprint->value( 'rioxx2_dateAccepted' ) );
-    }
-    if( $eprint->exists_and_set( 'rioxx2_publication_date' ) )
-    {
-        $eprint->set_value( 'hoa_date_pub', $eprint->value( 'rioxx2_publication_date' ) );
-    }
+	# rioxx2 - will prefer 'published' to 'published_online' - REF_CC prefers it the other way round
+	if( $eprint->exists_and_set( 'rioxx2_dateAccepted' ) )
+	{
+		$acc_date = $eprint->value( 'rioxx2_dateAccepted' );
+	}
+	if( $eprint->exists_and_set( 'rioxx2_publication_date' ) )
+	{
+		$pub_date = $eprint->value( 'rioxx2_publication_date' );
+	}
 
+	# now set the values - date of acceptance
+	$eprint->set_value( 'hoa_date_acc', $acc_date ) if defined $acc_date;
+	#prefer a published_online date
+	if( defined $pub_online_date )
+	{
+		$eprint->set_value( 'hoa_date_pub', $pub_online_date );
+	}
+	elsif( defined $pub_date ) #but use a published date if that's not available.
+	{
+		$eprint->set_value( 'hoa_date_pub', $pub_date );
+	}
+    
 }, priority => 100 ); # needs to be called before the compliance flag is set

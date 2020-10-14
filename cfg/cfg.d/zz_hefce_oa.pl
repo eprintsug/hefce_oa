40913b47b26cf473f3738f947d88a6468d2d17f7
Will Fyson <will.fyson@cosector.com>
2020-10-14 14:18:48 +0000

    Fix to prevent fcd from being reset if it is accidentally wiped by another system, update exception mapping trigger to copy deposit exception text, and update to reports to include new exceptions (along with tab/space formatting fixes

--- cfg/cfg.d/zz_hefce_oa.pl
+++ cfg/cfg.d/zz_hefce_oa.pl
@@ -42,135 +42,135 @@ $c->{hefce_oa}->{foa_retraction_period} = undef; #number of days
 # The value above is referenced in the following method, called by a commit trigger
 $c->{hefce_oa}->{commit_in_foa_retraction_period} = sub
 {
-	my( $repo, $date_foa ) = @_;
+    my( $repo, $date_foa ) = @_;
 
-	use Time::Piece;
-	use Time::Seconds;
+    use Time::Piece;
+    use Time::Seconds;
 
-	return unless defined $date_foa;
-	my $period = $repo->config( "hefce_oa", "foa_retraction_period" );
-	return unless defined $period;
+    return unless defined $date_foa;
+    my $period = $repo->config( "hefce_oa", "foa_retraction_period" );
+    return unless defined $period;
 
 
-	my $retraction_end = Time::Piece->strptime( $date_foa, "%Y-%m-%d"); #defaults to 00:00:00
-	$retraction_end += ONE_DAY * $period;
+    my $retraction_end = Time::Piece->strptime( $date_foa, "%Y-%m-%d"); #defaults to 00:00:00
+    $retraction_end += ONE_DAY * $period;
 
-	# older versions of Time::Piece don't have the 'truncate' method. The below should return
-	# a consistent date for comparisons
-	my $today = Time::Piece->strptime( EPrints::Time::get_iso_date(), "%Y-%m-%d" );
+    # older versions of Time::Piece don't have the 'truncate' method. The below should return
+    # a consistent date for comparisons
+    my $today = Time::Piece->strptime( EPrints::Time::get_iso_date(), "%Y-%m-%d" );
 
-	return 1 if $today <= $retraction_end;  
+    return 1 if $today <= $retraction_end;  
 };
 
 #set order of preference for export plugins (GRF v3.1)
 $c->{hefce_report}->{export_plugins} = [ qw( Export::Report::CSV Export::Report::HTML Export::Report::JSON )];
 
 $c->{hefce_report}->{exportfields} = {
-	ref_core => [ qw(
-        	eprintid
-                documents.content
-                type
-                title
-                abstract
-                creators
-                publisher
-		publication
-                divisions
-                dates
-                id_number
-                isbn
-                issn
- 	        official_url                                            
-	)],
-        ref_rioxx => [ qw(
-	       	rioxx2_free_to_read
-                rioxx2_license_ref
-                rioxx2_coverage
-                rioxx2_source
-                rioxx2_subject
-                rioxx2_dateAccepted
-                rioxx2_publication_date
-                rioxx2_apc
-                rioxx2_project
-                rioxx2_version
-                rioxx2_version_of_record
-	)],
-        ref_exceptions => [ qw(
-                hoa_compliant
-                hoa_problems
-		hoa_gold
-                hoa_ref_pan
-                hoa_ex_dep
-                hoa_ex_dep_txt
-                hoa_ex_acc
-                hoa_ex_acc_txt
-                hoa_ex_tec
-                hoa_ex_tec_txt
-                hoa_ex_oth
-                hoa_ex_oth_txt
-	)],
+    ref_core => [ qw(
+        eprintid
+        documents.content
+        type
+        title
+        abstract
+        creators
+        publisher
+        publication
+        divisions
+        dates
+        id_number
+        isbn
+        issn
+        official_url                                            
+    )],
+    ref_rioxx => [ qw(
+        rioxx2_free_to_read
+        rioxx2_license_ref
+        rioxx2_coverage
+        rioxx2_source
+        rioxx2_subject
+        rioxx2_dateAccepted
+        rioxx2_publication_date
+        rioxx2_apc
+        rioxx2_project
+        rioxx2_version
+        rioxx2_version_of_record
+    )],
+    ref_exceptions => [ qw(
+        hoa_compliant
+        hoa_problems
+        hoa_gold
+        hoa_ref_pan
+        hoa_ex_dep
+        hoa_ex_dep_txt
+        hoa_ex_acc
+        hoa_ex_acc_txt
+        hoa_ex_tec
+        hoa_ex_tec_txt
+        hoa_ex_fur
+        hoa_ex_fur_txt
+    )],
 };
 
 $c->{hefce_report}->{exportfield_defaults} = [ qw( eprintid documents.content type title creators dates hoa_compliant hoa_problems ) ];
 
 $c->{hefce_report}->{custom_export} = {
-        hoa_compliant => sub {
-		my( $dataobj, $plugin ) = @_;
+    hoa_compliant => sub {
+        my( $dataobj, $plugin ) = @_;
 
-                my $compliance = "Compliant";
+            my $compliance = "Compliant";
 
-                my $repo = $dataobj->repository;
+            my $repo = $dataobj->repository;
 
-                my $out_of_scope = $repo->call( [ "hefce_oa", "OUT_OF_SCOPE_reason" ], $repo, $dataobj );
+            my $out_of_scope = $repo->call( [ "hefce_oa", "OUT_OF_SCOPE_reason" ], $repo, $dataobj );
                 
-                if( $out_of_scope eq "acc" || $out_of_scope eq "pub" )
-                {
-                        return "Out of scope (Pre April 2016)";
-                }
-		elsif( $out_of_scope eq "gold" )
-                {
-                        return "Out of scope (Gold OA)";
-                }
-		elsif( $out_of_scope eq "issn" )
-		{
-			return "Out of scope (no ISSN)";
-		}
+            if( $out_of_scope eq "acc" || $out_of_scope eq "pub" )
+            {
+                return "Out of scope (Pre April 2016)";
+            }
+            elsif( $out_of_scope eq "gold" )
+            {
+                return "Out of scope (Gold OA)";
+            }
+            elsif( $out_of_scope eq "issn" )
+            {
+                return "Out of scope (no ISSN)";
+            }
 
-                my @problems = $plugin->validate_dataobj( $dataobj );
-                if( scalar( @problems ) > 0 )
-                {
-                        $compliance = "Not Compliant";
-                }
+            my @problems = $plugin->validate_dataobj( $dataobj );
+            if( scalar( @problems ) > 0 )
+            {
+                $compliance = "Not Compliant";
+            }
 
-                my $state = $plugin->get_state( $dataobj );
-                if( defined $state && $state eq "#E19141" )
-                {
-                        $compliance = "Compliant pending open access";
-                }
-                return $compliance;
+            my $state = $plugin->get_state( $dataobj );
+            if( defined $state && $state eq "#E19141" )
+            {
+                $compliance = "Compliant pending open access";
+            }
+            return $compliance;
         },
         creators => sub {
-        	my( $dataobj ) = @_;
+            my( $dataobj ) = @_;
 
-                my @creator_names;
-                my $creators = $dataobj->get_value( "creators" );
-                foreach my $c ( @{$creators} )
-                {
-                	push @creator_names, EPrints::Utils::make_name_string( $c->{name} );
-	        }
-                return join( ";", @creator_names );
-	},
+            my @creator_names;
+            my $creators = $dataobj->get_value( "creators" );
+            foreach my $c ( @{$creators} )
+            {
+                push @creator_names, EPrints::Utils::make_name_string( $c->{name} );
+            }
+            return join( ";", @creator_names );
+        },
         dates => sub {
-		my( $dataobj ) = @_;
+            my( $dataobj ) = @_;
 
-                my @date_strings;
-                my $dates = $dataobj->get_value( "dates" );
-                foreach my $d ( @{$dates} )
-                {
-			push @date_strings, $d->{date} . " (" . $d->{date_type} . ")";
-                }
-                return join( ";", @date_strings );
-	},
+            my @date_strings;
+            my $dates = $dataobj->get_value( "dates" );
+            foreach my $d ( @{$dates} )
+            {
+                push @date_strings, $d->{date} . " (" . $d->{date_type} . ")";
+            }
+            return join( ";", @date_strings );
+        },
         rioxx2_free_to_read => sub {
             my( $dataobj ) = @_;
     
@@ -216,36 +216,39 @@ $c->{hefce_report}->{custom_export} = {
 ####################################################
 $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 {
-	my( %args ) = @_;
-	my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
+    my( %args ) = @_;
+    my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
 
-	# Deposit exception (g) --> Further exception (b)
-	if( ( $eprint->is_set( "hoa_ex_dep" ) && $eprint->get_value( "hoa_ex_dep" ) eq "g" ) ||
-   		( exists $changed->{hoa_ex_dep} && $changed->{hoa_ex_dep} eq "g" ) )
-	{
-		$eprint->set_value( "hoa_ex_dep", undef ); #unset existing value
-		$eprint->set_value( "hoa_ex_fur", "b" );
-	}
+    # Deposit exception (g) --> Further exception (b)
+    if( ( $eprint->is_set( "hoa_ex_dep" ) && $eprint->get_value( "hoa_ex_dep" ) eq "g" ) ||
+        ( exists $changed->{hoa_ex_dep} && $changed->{hoa_ex_dep} eq "g" ) )
+    {
+        $eprint->set_value( "hoa_ex_dep", undef ); #unset existing value
+        $eprint->set_value( "hoa_ex_fur", "b" );
+        if( $eprint->is_set( "hoa_ex_dep_txt" ) )
+        {
+            $eprint->set_value( "hoa_ex_fur_txt", $eprint->get_value( "hoa_ex_dep_txt" ) );
+            $eprint->set_value( "hoa_ex_dep_txt", undef );
+        }
+    }
 
-	# Deposit exception (f) --> Gold OA (hoa_gold)
-	if( ( $eprint->is_set( "hoa_ex_dep" ) && $eprint->get_value( "hoa_ex_dep" ) eq "f" ) ||
-   		( exists $changed->{hoa_ex_dep} && $changed->{hoa_ex_dep} eq "f" ) )
-	{
-		$eprint->set_value( "hoa_ex_dep", undef );
-		$eprint->set_value( "hoa_gold", "TRUE" );
-	}	
+    # Deposit exception (f) --> Gold OA (hoa_gold)
+    if( ( $eprint->is_set( "hoa_ex_dep" ) && $eprint->get_value( "hoa_ex_dep" ) eq "f" ) ||
+        ( exists $changed->{hoa_ex_dep} && $changed->{hoa_ex_dep} eq "f" ) )
+    {
+        $eprint->set_value( "hoa_ex_dep", undef );
+        $eprint->set_value( "hoa_gold", "TRUE" );
+    }
 
-	# Other exception --> Further exception (a)
-	if( ( $eprint->is_set( "hoa_ex_oth" ) && $eprint->get_value( "hoa_ex_oth" ) eq "TRUE" ) ||
-   		( exists $changed->{hoa_ex_oth} && $changed->{hoa_ex_oth} eq "TRUE" ) )
-	{
-		$eprint->set_value( "hoa_ex_oth", undef );
-		$eprint->set_value( "hoa_ex_fur", "a" );
-		if( $eprint->is_set( "hoa_ex_oth_txt" ) )
-		{
-			$eprint->set_value( "hoa_ex_fur_txt", $eprint->get_value( "hoa_ex_oth_txt" ) );
-		}
-	}
-	
+    # Other exception --> Further exception (a)
+    if( ( $eprint->is_set( "hoa_ex_oth" ) && $eprint->get_value( "hoa_ex_oth" ) eq "TRUE" ) ||
+        ( exists $changed->{hoa_ex_oth} && $changed->{hoa_ex_oth} eq "TRUE" ) )
+    {
+        $eprint->set_value( "hoa_ex_oth", undef );
+        $eprint->set_value( "hoa_ex_fur", "a" );
+        if( $eprint->is_set( "hoa_ex_oth_txt" ) )
+        {
+            $eprint->set_value( "hoa_ex_fur_txt", $eprint->get_value( "hoa_ex_oth_txt" ) );
+        }
+    }
 }, priority => 50 );
-
--- lib/cfg.d/hefce_oa_triggers.pl
+++ lib/cfg.d/hefce_oa_triggers.pl
@@ -22,46 +22,46 @@ $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 # date of first compliant deposit
 $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 {
-	my( %args ) = @_; 
-	my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
+    my( %args ) = @_; 
+    my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
 
-	# trigger only applies to repos with hefce_oa plugin enabled
-	return unless $eprint->dataset->has_field( "hoa_compliant" );
+    # trigger only applies to repos with hefce_oa plugin enabled
+    return unless $eprint->dataset->has_field( "hoa_compliant" );
 
-	return if $eprint->is_set( "hoa_date_fcd" );
-	return if $eprint->value( "eprint_status" ) eq "inbox";
+    return if $eprint->is_set( "hoa_date_fcd" );
+    return if $eprint->value( "eprint_status" ) eq "inbox";
 
-	for( $eprint->get_all_documents )
-	{
-		next unless $_->is_set( "content" );
-		my $content = $_->value( "content" );
-		next unless grep( /^$content$/, @{$repo->config( "hefce_oa", "document_content" )});
-    		$eprint->set_value( "hoa_date_fcd", EPrints::Time::get_iso_date() );
-    		$eprint->set_value( "hoa_version_fcd", $content eq "published" ? "VoR" : "AM" );
-	}
+    for( $eprint->get_all_documents )
+    {
+        next unless $_->is_set( "content" );
+        my $content = $_->value( "content" );
+        next unless grep( /^$content$/, @{$repo->config( "hefce_oa", "document_content" )});
+        $eprint->set_value( "hoa_date_fcd", defined $changed->{'hoa_date_fcd'} ? $changed->{'hoa_date_fcd'} : EPrints::Time::get_iso_date() ); # in the event fcd has been wiped by something, retrieve it from the $changed value
+        $eprint->set_value( "hoa_version_fcd", $content eq "published" ? "VoR" : "AM" );
+    }
 }, priority => 100 );
 
 # date of first compliant open access
 $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 {
-	my( %args ) = @_; 
-	my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
+    my( %args ) = @_; 
+    my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
 
-	# trigger only applies to repos with hefce_oa plugin enabled
-	return unless $eprint->dataset->has_field( "hoa_compliant" );
+    # trigger only applies to repos with hefce_oa plugin enabled
+    return unless $eprint->dataset->has_field( "hoa_compliant" );
 
-	return unless $eprint->is_set( "hoa_date_fcd" );
-	return if $eprint->is_set( "hoa_date_foa" );
+    return unless $eprint->is_set( "hoa_date_fcd" );
+    return if $eprint->is_set( "hoa_date_foa" );
 
-	for( $eprint->get_all_documents )
-	{
-		next unless $_->is_set( "content" );
-		my $content = $_->value( "content" );
-		next unless grep( /^$content$/, @{$repo->config( "hefce_oa", "document_content" )} );
-		next unless $_->is_public;
-		# NB $changed has the *old* values in.
-		$eprint->set_value( "hoa_date_foa", defined $changed->{'hoa_date_foa'} ? $changed->{'hoa_date_foa'} : EPrints::Time::get_iso_date() );
-	}
+    for( $eprint->get_all_documents )
+    {
+        next unless $_->is_set( "content" );
+        my $content = $_->value( "content" );
+        next unless grep( /^$content$/, @{$repo->config( "hefce_oa", "document_content" )} );
+        next unless $_->is_public;
+        # NB $changed has the *old* values in.
+        $eprint->set_value( "hoa_date_foa", defined $changed->{'hoa_date_foa'} ? $changed->{'hoa_date_foa'} : EPrints::Time::get_iso_date() );
+    }
 }, priority => 200 );
 
 # Sometimes a document is open-access fleetingly.
@@ -69,68 +69,68 @@ $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 # in the repository configuration. See:  zz_hefce_oa.pl
 $c->add_dataset_trigger( 'document', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 {
-	my( %args ) = @_; 
-	my( $repo, $doc, $changed ) = @args{qw( repository dataobj changed )};
+    my( %args ) = @_; 
+    my( $repo, $doc, $changed ) = @args{qw( repository dataobj changed )};
 
-	# trigger only applies to repos with hefce_oa plugin enabled
-	return unless $doc->parent->dataset->has_field( "hoa_compliant" );
+    # trigger only applies to repos with hefce_oa plugin enabled
+    return unless $doc->parent->dataset->has_field( "hoa_compliant" );
 
-	if( $doc->is_public )
-	{
-		return if $doc->parent->is_set( "hoa_date_foa" );
-	}
-	else
-	{
-		return unless $doc->parent->is_set( "hoa_date_foa" );
-		return unless $repo->can_call( "hefce_oa", "commit_in_foa_retraction_period" );
+    if( $doc->is_public )
+    {
+        return if $doc->parent->is_set( "hoa_date_foa" );
+    }
+    else
+    {
+        return unless $doc->parent->is_set( "hoa_date_foa" );
+        return unless $repo->can_call( "hefce_oa", "commit_in_foa_retraction_period" );
 
-		return unless $repo->call( [ "hefce_oa", "commit_in_foa_retraction_period" ], $repo, $doc->parent->value( "hoa_date_foa" ) );
-		$doc->parent->set_value( "hoa_date_foa", undef );
-	}
+        return unless $repo->call( [ "hefce_oa", "commit_in_foa_retraction_period" ], $repo, $doc->parent->value( "hoa_date_foa" ) );
+        $doc->parent->set_value( "hoa_date_foa", undef );
+    }
 
-	# make sure eprint->commit calls triggers..
-	# see https://github.com/eprintsug/hefce_oa/issues/19
-	$doc->parent->{changed}->{hoa_update_ep}++;
+    # make sure eprint->commit calls triggers..
+    # see https://github.com/eprintsug/hefce_oa/issues/19
+    $doc->parent->{changed}->{hoa_update_ep}++;
 }, priority => 100 );
 
 # set compliance flag
 $c->add_dataset_trigger( 'eprint', EPrints::Const::EP_TRIGGER_BEFORE_COMMIT, sub
 {
-	my( %args ) = @_;
-	my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
+    my( %args ) = @_;
+    my( $repo, $eprint, $changed ) = @args{qw( repository dataobj changed )};
 
-	# trigger only applies to repos with hefce_oa plugin enabled
-	return unless $eprint->dataset->has_field( "hoa_compliant" );
+    # trigger only applies to repos with hefce_oa plugin enabled
+    return unless $eprint->dataset->has_field( "hoa_compliant" );
 
-	my $type = $eprint->value( "type" );
+    my $type = $eprint->value( "type" );
 
-	unless( defined $type && grep( /^$type$/, @{$repo->config( "hefce_oa", "item_types" )} ) )
-	{		
-		$eprint->set_value( "hoa_compliant", undef );
-		return;
-	}
+    unless( defined $type && grep( /^$type$/, @{$repo->config( "hefce_oa", "item_types" )} ) )
+    {
+        $eprint->set_value( "hoa_compliant", undef );
+        return;
+    }
 
-	my $flag = 0;
-	for(qw(
-		DEP_COMPLIANT
-		DEP_TIMING
-		DEP
-		DIS_DISCOVERABLE
-		DIS
-		ACC_TIMING
-		ACC_EMBARGO
-		ACC
-		EX_DEP
-		EX_ACC
-		EX_TEC
-		EX_FUR
-		EX
-		COMPLIANT
-	))
-	{
-		$flag |= HefceOA::Const->$_ if $repo->call( [qw( hefce_oa run_test )], $repo, $_, $eprint, $flag );
-	}
+    my $flag = 0;
+    for(qw(
+        DEP_COMPLIANT
+        DEP_TIMING
+        DEP
+        DIS_DISCOVERABLE
+        DIS
+        ACC_TIMING
+        ACC_EMBARGO
+        ACC
+        EX_DEP
+        EX_ACC
+        EX_TEC
+        EX_FUR
+        EX
+        COMPLIANT
+    ))
+    {
+        $flag |= HefceOA::Const->$_ if $repo->call( [qw( hefce_oa run_test )], $repo, $_, $eprint, $flag );
+    }
 
-	$eprint->set_value( "hoa_compliant", $flag );
+    $eprint->set_value( "hoa_compliant", $flag );
 
 }, priority => 300 );
